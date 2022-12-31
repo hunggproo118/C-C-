8cd840c6c84f506065d7b1fb0d804f4955be1be9
Ducknight <121716931+hunggproo118@users.noreply.github.com>
2022-12-31 22:35:33 +0700

    Update Ex 4.cpp

--- Ex 4.cpp
+++ Ex 4.cpp
@@ -4,13 +4,8 @@
 int main(){
 	unsigned int k,n;
 	scanf("%d", &n);
-	if (n<=2*k<=2*n){
-		printf("%d", k=round((n+1)/2));
-	}else{
-		printf("ko thoa man de bai");
-	}
-}
-	
+	printf("%d", k=round((n+1)/2));
+}	
 	
 	
 	
